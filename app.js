0dff496769477c514d8d2204ffdb099e15d147ad
Tolulope Folorunso <tolufolorunso@yahoo.com>
2020-07-24 15:51:29 +0100

    Simple server without frameworks

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ const app = http.createServer();
 const PORT = 3000;
 
 app.on("request", (req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/html",
-  });
   const message = "<h1>Hello World, welcome to WeJapa Internships</h1>";
-  res.end(message);
+  const notFound = "<h1><h2>Page not found</h2>";
+  if (req.url === "/") {
+    res.writeHead(200, {
+      "Content-Type": "text/html",
+    });
+    res.end(message);
+  } else {
+    res.writeHead(404, {
+      "Content-Type": "text/html",
+    });
+    res.end(notFound);
+  }
 });
 
 app.listen(PORT, () => {
